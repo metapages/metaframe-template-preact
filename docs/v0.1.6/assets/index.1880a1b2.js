b969ba1653c6cd8b933119785504f75ee9f86008
robot <ci@rob.ot>
2021-09-10 20:38:31 +0000

    site v0.1.6

---
+++ docs/v0.1.6/assets/index.1880a1b2.js
@@ -0,0 +1,2 @@
+import{F as n,M as a,a as e,B as r,N as l,W as o,C as m}from"./vendor.ffaeee36.js";const s=()=>{const t=n(a);return e("div",null,e(r,null,"metaframe inputs:")," ",t?JSON.stringify(t.inputs):"none yet")},u=()=>e(s,null);l(e(m,null,e(o,null,e(u,null))),document.getElementById("root"));
+//# sourceMappingURL=index.1880a1b2.js.map
